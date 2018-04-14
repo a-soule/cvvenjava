c0865a01adc666d7bb5fcd81adc0b1f7ab0252ab
pc <pc@DESKTOP-Q186BT1>
2018-04-14 21:39:58 +0200

    Ajout Connexion, Inscription, Ajout Event et Afficher Event

--- src/hibernatecvven/gestion/MainCVVEN.java
+++ src/hibernatecvven/gestion/MainCVVEN.java
@@ -115,6 +115,7 @@ public class MainCVVEN extends javax.swing.JFrame {
         fr.setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
+
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         InsererEvents fr = new InsererEvents();
         fr.setVisible(true);
